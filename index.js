f9bf3d28b95b0763182b735b3378c4e98ff8ca7e
tushar-dev001 <tushar789imran@gmail.com>
2023-11-15 02:48:56 +0600

    cart query data create and cart delete data create

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
@@ -35,10 +35,39 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("tsrDB").collection("menu");
+    const reviewCollection = client.db("tsrDB").collection("reviews");
+    const cartCollection = client.db("tsrDB").collection("carts");
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray()
         res.send(result)
+    })
+
+    app.get('/reviews', async(req, res)=>{
+        const result = await reviewCollection.find().toArray()
+        res.send(result)
+    })
+
+    //carts collection
+    app.get('/carts', async(req, res)=>{
+        const email = req.query.email;
+        const query = {email: email}
+        const result = await cartCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    //user jokhon add to cart korte caibe tokhon aeta use korte hobe
+    app.post('/carts', async(req, res)=>{
+        const cartItem = req.body;
+        const result = await cartCollection.insertOne(cartItem)
+        res.send(result)
+    })
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
     })
 
 
