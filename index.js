55ed468a3d037d33c07d679c482cf87be87093b7
tushar-dev001 <tushar789imran@gmail.com>
2023-12-12 02:31:31 +0600

    admin page desing start

--- index.js
+++ index.js
@@ -220,29 +220,108 @@ async function run() {
       });
     });
 
-    app.get('/payments/:email', verifyToken, async(req, res)=>{
-      const query = {email: req.params.email}
-      if(req.params.email !== req.decoded.email){
-        return res.status(403).send({message: 'forbidden access'})
+    app.get("/payments/:email", verifyToken, async (req, res) => {
+      const query = { email: req.params.email };
+      if (req.params.email !== req.decoded.email) {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      const result = await paymentCollection.find(query).toArray()
-      res.send(result)
-    })
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/payments', async(req, res)=>{
+    app.post("/payments", async (req, res) => {
       const payment = req.body;
-      const paymentResult = await paymentCollection.insertOne(payment)
+      const paymentResult = await paymentCollection.insertOne(payment);
 
       //carefully delete each item from the cart
-      console.log('payment info', payment);
-      const query = {_id: {
-        $in: payment.cartIds.map(id => new ObjectId(id))
-      }}
+      console.log("payment info", payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map((id) => new ObjectId(id)),
+        },
+      };
 
-      const deleteResult = await cartCollection.deleteMany(query)
+      const deleteResult = await cartCollection.deleteMany(query);
 
-      res.send({paymentResult, deleteResult})
-    })
+      res.send({ paymentResult, deleteResult });
+    });
+
+    //stats or analytics
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const menuItems = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      //this is not the best way
+      // const payments = await paymentCollection.find().toArray()
+      // const revenue = payments.reduce((total, payment) => total + payment.price, 0)
+
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({
+        users,
+        menuItems,
+        orders,
+        revenue,
+      });
+    });
+
+    /**
+     * ---------------------------------
+     * NON-Efficient Way
+     * ---------------------------------
+     * 1. load all the payments
+     * 2. for every menuItems (which is an array), go find the item from menu collection
+     * 3. for every item in the menu collection that you found from a payment entry (document)
+     *
+     * */
+
+    //using aggregate pipeline
+    app.get("/order-stats", async (req, res) => {
+        const result = await paymentCollection.aggregate([
+          {
+            $unwind: "$menuItemIds",
+          },
+          {
+            $lookup: {
+              from: "menu",
+              localField: "menuItemIds",
+              foreignField: "_id",
+              as: "menuItems",
+            },
+          },
+          {
+            $unwind: "$menuItems",
+          },
+          {
+            $group: {
+              _id: "$menuItems.category",
+              quantity: { $sum: 1 },
+              revenue: { $sum: "$menuItems.price" },
+            },
+          },
+          
+        ]).toArray();
+
+        res.send(result);
+
+
+      })
+      
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
